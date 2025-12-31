8b355ea5cede853688663fd39a5e830dc39b9b78
Camila F. B. Coelho <camilafernanda914@gmail.com>
2025-12-31 14:48:53 -0300

    build: atualiza react2shell para corrigir problema de vulnerabilidade

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -23,7 +23,7 @@ export const Projects = () => {
           <h2 className="text-center text-3xl md:text-4xl lg:text-5xl font-bold mb-4 font-space text-foreground">
             Projetos
           </h2>
-          <p className="mb-8 text-foreground-muted max-w-2xl text-center text-sm lg:text-lg">
+          <p className="mb-8 text-foreground-muted max-w-2xl text-center text-sm lg:text-lg mx-auto">
             Alguns dos projetos que desenvolvi, demonstrando minhas habilidades
             e experiência em desenvolvimento web
           </p>
